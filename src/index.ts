a4420f9b8b36b7f902f89bb2b33de1fde50e80f6
Amin Bakhtiari Far <amin.bakhtiarifar@gmail.com>
2021-01-26 14:59:08 +0330

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+// Update
+
 export * from "./errors/custom-error"
 export * from "./errors/database-connection-error"
 export * from "./errors/not-authenticated-error"
 export * from "./errors/not-authorized-error"
 export * from "./errors/not-found-error"
 export * from "./errors/request-validation-error"
+
 export * from "./middlewares/current-user"
 export * from "./middlewares/error-handler"
 export * from "./middlewares/request-validation"
